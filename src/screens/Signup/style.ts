ced0dc041d9a3d1431873dabafe2b0b0f8513211
Rodrigo Mello <rodrigojcmello@gmail.com>
2019-07-07 16:05:20 -0300

    email margin top decreased

--- src/screens/Signup/style.ts
+++ src/screens/Signup/style.ts
@@ -1,7 +1,7 @@
 import styled from '../../assets/styled-components';
 
 export const Content = styled.div`
-  margin: 30vh auto 0 auto;
+  margin: 15vh auto 0 auto;
   max-width: 400px;
   padding: 8vw;
 `;
